00770dc4b27cab1becaf6a2dd29dd40c6d5a6ef3
Jimmy <feltsadas.j@gmail.com>
2016-02-28 13:01:14 +0100

    jobData naar vector gezet

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,4 +1,40 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
 int main(int argc, char **argv)
 {
-	return 0;
+	std::vector<std::string> jobs;
+	std::string jobMachineLine;
+	std::string line;
+	std::ifstream jobData ("src/jobData.txt");
+
+	if (jobData.is_open())
+	{
+		std::getline(jobData, jobMachineLine);
+		std::cout << jobMachineLine << std::endl;
+
+		for (int i = 0; i < (int)jobMachineLine[0] - '0'; i++)
+		{
+			std::string buffer;
+			std::getline(jobData, buffer);
+			jobs.push_back(buffer);
+		}
+		jobData.close();
+	}
+	else
+	{
+		std::cout << "Unable to open file" << std::endl;
+	}
+
+
+	for (const std::string n : jobs)
+	{
+		std::cout << n << std::endl;
+	}
+
+	std::cout << jobs.size() << std::endl;
+
+	  return 0;
 }
